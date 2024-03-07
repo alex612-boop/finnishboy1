c83efb721df65d1e55d3d926816e4d3d97ce8149
alex612-boop <tw1x1kwow@ukr.net>
2024-03-07 23:27:40 +0000

    Added helper function

---
+++ index.js
@@ -0,0 +1,11 @@
+// Helper function to calculate the area of a rectangle
+function calculateRectangleArea(width, height) {
+    return width * height;
+  }
+  
+  // Main code to use the helper function
+  const width = 5; // You can change the width of the rectangle
+  const height = 8; // You can change the height of the rectangle
+  const area = calculateRectangleArea(width, height);
+  console.log(`The area of the rectangle with width ${width} and height ${height} is ${area}`);
+  
